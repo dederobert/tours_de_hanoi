6abf20b715b135b256628444555bf1241d101ec3
dederobert <adinquer@yahoo.com>
2019-01-12 02:57:09 +0100

    Updates

---
+++ precache-manifest.dc47d6f21c35ca8c616f746ed9a4ab67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "009c1c3e56360bdfa9a2",
+    "url": "/tours_de_hanoi/static/js/runtime~main.009c1c3e.js"
+  },
+  {
+    "revision": "da22cdd52f8f4a48813d",
+    "url": "/tours_de_hanoi/static/js/main.da22cdd5.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/tours_de_hanoi/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "da22cdd52f8f4a48813d",
+    "url": "/tours_de_hanoi/static/css/main.8bd54da7.chunk.css"
+  },
+  {
+    "revision": "20c147c529f8f3a180ad35760f996f0d",
+    "url": "/tours_de_hanoi/index.html"
+  }
+];
